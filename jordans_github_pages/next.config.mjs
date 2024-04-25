e3fb8a36a023168395e5b227f5b7df0d23fd01bd
JordanClough <jordan_clough@sfu.ca>
2024-04-25 13:52:53 -0700

    testing

--- jordans_github_pages/next.config.mjs
+++ jordans_github_pages/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/{jordans_github_pages}' : '',
+  };
+  
+  //module.exports = nextConfig;
+  export default nextConfig;
